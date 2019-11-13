c16823007b001c742ad836d9a526ac655baa1423
chad svastisalee <swasgrafx@gmail.com>
2019-11-13 13:47:28 -0600

    Added Articles Schema, Articles Service, Article Component

--- src/app/user-view/user-view.component.ts
+++ src/app/user-view/user-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { UsersService } from '../users.service';
 import { User } from '../user';
@@ -15,6 +16,7 @@ export class UserViewComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private usersService: UsersService
   ) { }
 
@@ -29,5 +31,13 @@ export class UserViewComponent implements OnInit {
         this.user = response.user
       }
     );
+  }
+  //3. Implement the deleteUser() method
+  deleteUser(id: string): void {
+    if(confirm("Are you sure to delete " + this.user.username)) {
+      this.usersService.deleteUser(id).subscribe(
+        ()=>{this.router.navigate(['/users'])}
+      );
+    }
   }
 }
