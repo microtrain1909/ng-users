9be0dbc0fcf810528aeeb7c33423c7676d32ef0f
chad svastisalee <swasgrafx@gmail.com>
2019-11-14 21:52:06 -0600

    Added the ability to delete an Article

--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -33,4 +33,7 @@ export class ArticlesService {
     return this.http.put<Article>(this.url, article, httpOptions);
   }
 
+  deleteArticle (id: string): Observable<Article> {
+    return this.http.delete<Article>(`${this.url}/${id}`);
+  }
 }
